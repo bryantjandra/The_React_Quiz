7838aed8feb65eee7e9d3542a893d575510329ee
Bryan Tjandra <tjandra.benedict@gmail.com>
2023-08-12 02:06:23 +0700

    implement timer and highscore

---
+++ src/components/Timer.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+
+export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+      return () => clearInterval(id); //to cancel the timer whenever the component is unmounted. This is so that the timer will actually stop.
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
